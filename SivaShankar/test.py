1d80d4ad18e5edf29ab45b6ce0811df55fa31c55
Siva Shankar Erumala <sivashankar123abc@gmail.com>
2022-03-21 17:51:13 +0530

    second commit by SS

--- SivaShankar/test.py
+++ SivaShankar/test.py
@@ -1,2 +1,9 @@
 print("Hello........")
-def printer():
+
+def printer(a):
+
+    print(f"I'm {a}. Currently working as a Trainee software engineer in Bourntec Sol. ")
+
+a = input(" enter the name of employee  : \n")
+
+printer(a)
