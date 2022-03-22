210b5f48f6d52cc7245224c7ee6d07dfa3bfffea
Siva Shankar Erumala <sivashankar123abc@gmail.com>
2022-03-22 11:04:05 +0530

    Done:Relative path and Absolute path to refer file

--- SivaShankar/realtivePath_absolutePath.py
+++ SivaShankar/realtivePath_absolutePath.py
@@ -1,9 +1,15 @@
-print("hello world")
 import os
 # print(os.getcwd())
 # print(os.listdir(os.getcwd()))
+
+# change the dir 
 os.chdir('SivaShankar/featureExtraction')
 print(os.getcwd())
+# using Relative path 
+# ../ => it get backs to the previous dir
 import pandas as pd
 df = pd.read_csv('../../dataset/1.csv',encoding = 'utf-8')
 print(df.head())
+
+# using absolute path to read files.
+# D:/ailearning/aiLearning/dataset/1.csv
