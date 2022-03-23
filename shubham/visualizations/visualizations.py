43c1bcb1c2c623d8694833035683f431a1707763
ShubhamSurya_ljtsrll <shubham.sirsat321@gmail.com>
2022-03-23 09:49:35 +0530

    done : visualization , only two plot

--- shubham/visualizations/visualizations.py
+++ shubham/visualizations/visualizations.py
@@ -1,9 +1,20 @@
 import pandas as pd
 import glob
+import matplotlib.pyplot as plt
+import zipfile
 import os
 print(os.getcwd())
-path = "aiLearning\\dataset\\"
-files = glob.glob(path + "/*.csv")
-# df = pd.read_csv(glob.glob("1.sv"c))
 
-print(files)
+unzipfile = zipfile.ZipFile("visualization.zip")
+
+zipcsv = unzipfile.open('visualization.csv')
+
+df = pd.read_csv(zipcsv)
+
+print(df)
+
+plt.bar(df["Type"].unique(),df['Type'].value_counts())
+plt.show()
+
+plt.pie(df['Type'].value_counts(),labels=df["Type"].unique())
+plt.show()
