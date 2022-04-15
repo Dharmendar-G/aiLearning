0b979f263ca32bcdf6649c6e7f92ccdc878a0e57
Dharmendar-G <dharmendra.gandi@gmail.com>
2022-04-15 08:26:35 +0530

    done : cve data collection

--- dharmendra/data collection/cve_data_collection.py
+++ dharmendra/data collection/cve_data_collection.py
@@ -1,3 +1,4 @@
+import matplotlib.pyplot as plt
 import pandas as pd
 import requests
 from bs4 import BeautifulSoup
@@ -5,11 +6,21 @@ from bs4 import BeautifulSoup
 url = 'https://www.cvedetails.com/'
 
 page = requests.get(url)
-tables = pd.read_html(page.content)
-c1, c2 = 'Number Of Vulnerabilities','CVSS Score'
-table = [t for t in tables if c1 and c2 in t][0]
+table = pd.read_html(page.content)[6]
 data = pd.DataFrame(table[:10])
-
+for c in data.columns:
+    try:
+        data[c] = data[c].astype('float')
+    except:
+        continue
+print(data)
 data.to_csv('cve_data.csv', index=False)
 
-print(data)
+# Visualization for vulnerabilities distribution 
+ax = data.plot(kind='bar', figsize=(10,6), 
+               title='Vulnerability Distribution by CVSS Scores', 
+               ylabel=data.columns[1], legend=False)
+ax.margins(y=0.1)
+ax.set_xticklabels(ax.get_xticklabels(), rotation=0)
+ax.bar_label(ax.containers[0])
+plt.show();
