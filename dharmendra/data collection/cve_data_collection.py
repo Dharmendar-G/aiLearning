86973b7de3d71802eb3f38250546f9a9bac0ec22
Dharmendar-G <dharmendra.gandi@gmail.com>
2022-04-18 09:15:27 +0530

    done : cve data collection

--- dharmendra/data collection/cve_data_collection.py
+++ dharmendra/data collection/cve_data_collection.py
@@ -3,15 +3,15 @@ import pandas as pd
 import requests
 from bs4 import BeautifulSoup
 
+# website
 url = 'https://www.cvedetails.com/'
-
 page = requests.get(url)
 table = pd.read_html(page.content)[6]
 data = pd.DataFrame(table[:10])
 for c in data.columns:
     try:
         data[c] = data[c].astype('float')
-    except:
+    except Exception as e:
         continue
 print(data)
 data.to_csv('cve_data.csv', index=False)
