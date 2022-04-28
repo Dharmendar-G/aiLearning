6a92613b4443fa47bb4bf759f0207e8147041f0e
Dharmendar-G <dharmendra.gandi@gmail.com>
2022-04-28 20:05:16 +0530

    done : visualization with python in power bi

---
+++ dharmendra/Power Bi Tasks/Graphs/graph_code.py
@@ -0,0 +1,12 @@
+import networkx as nx 
+import matplotlib.pyplot as plt 
+
+G = nx.DiGraph()
+G.add_edges_from([('A', 'B'), ('A', 'C'), ('C', 'B'), ('D', 'A')])
+val_map = {'A': 1.00,
+           'D': 0.00}
+values = [val_map.get(nodes, 0.50) for nodes in G.nodes()]
+pos = nx.spring_layout(G)
+nx.draw_networkx_nodes(G, pos, cmap=plt.get_cmap('Greens'), node_size=500, node_color=values)
+nx.draw_networkx_edges(G, pos, edgelist=G.edges(), edge_color='black')
+nx.draw_networkx_labels(G, pos)
